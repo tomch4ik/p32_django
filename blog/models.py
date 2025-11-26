54167284c23667e92ab5422604cb6d647bdc2ee6
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2025-11-26 17:46:35 +0200

    first model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
