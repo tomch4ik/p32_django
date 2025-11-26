979c9eb72f7df80796a9dafd9a838d02cd9b26e8
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2025-11-26 18:38:15 +0200

    add posts to site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_published", "create_at")
+    list_filter = ("is_published", "create_at")
+    search_fields = ("title", "content")
+ 
